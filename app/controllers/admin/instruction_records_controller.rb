04eb20c4bf7003440ab01232656aed1f75e55a64
08gucchama11 <296365775+08gucchama11@users.noreply.github.com>
2026-08-30 01:37:35 +0000

    [Add]管理者用指導記録閲覧機能作成

--- app/controllers/admin/instruction_records_controller.rb
+++ app/controllers/admin/instruction_records_controller.rb
@@ -1,4 +1,22 @@
 class Admin::InstructionRecordsController < Admin::ApplicationController
   def index
+    if params[:teacher_id].present?
+      @teacher = Teacher.find(params[:teacher_id])
+
+      @instruction_records = @teacher.instruction_records
+                                      .includes(:student)
+                                      .order(created_at: :desc)
+
+    elsif params[:student_id].present?
+      @student = Student.find(params[:student_id])
+
+      @instruction_records = @student.instruction_records
+                                      .includes(:teacher)
+                                      .order(created_at: :desc)
+
+    else
+      @instruction_records = InstructionRecord.includes(:student, :teacher)
+                                              .order(created_at: :desc)
+    end
   end
 end
