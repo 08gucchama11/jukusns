e3c46dc2f02fe40c37f0e45947dfd928e1241a8e
08gucchama11 <296365775+08gucchama11@users.noreply.github.com>
2026-09-09 23:11:48 +0000

    [Add]管理者のSNS投稿管理機能

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,10 +1,19 @@
 class Admin::PostsController < Admin::ApplicationController
   def index
+    @posts = Post.includes(:sns_profile).order(created_at: :desc)
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def destroy
+    @post = Post.find(params[:id])
+
+    if @post.destroy
+      redirect_to admin_posts_path, notice: "投稿を削除しました。"
+    else
+      redirect_to admin_posts_path, alert: "投稿を削除できません。"
+    end
   end
 end
