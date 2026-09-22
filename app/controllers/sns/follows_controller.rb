d8f8e5bc04d1671528e892732390ffb67eb5d33f
08gucchama11 <296365775+08gucchama11@users.noreply.github.com>
2026-09-22 23:15:55 +0000

    [Add]フォロー・フォロー解除ボタン

--- app/controllers/sns/follows_controller.rb
+++ app/controllers/sns/follows_controller.rb
@@ -3,8 +3,15 @@ class Sns::FollowsController < Sns::ApplicationController
   end
 
   def create
+    @sns_profile = SnsProfile.find(params[:profile_id])
+    @follow = current_sns_profile.following_relationships.find_or_create_by!(followed_sns_profile: @sns_profile)
+    redirect_back fallback_location: sns_root_path
   end
 
   def destroy
+    @sns_profile = SnsProfile.find(params[:profile_id])
+    @follow = current_sns_profile.following_relationships.find_by!(followed_sns_profile: @sns_profile)
+    @follow.destroy!
+    redirect_back fallback_location: sns_root_path
   end
 end
